3b384d2ee89e80d294787b45db3311975156126f
ahmed <ahmednagy@kortobaa.com>
2022-05-28 00:48:30 +0200

    test number trivia

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,7 @@
+import 'package:tdd_clean_arc/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  const GetConcreteNumberTrivia(this.repository);
+}
